94e87cb20d987484d1e4317ae99917dfaa0dad0d
Mohamed TOUIL <simotouil88@gmail.com>
2018-10-30 16:13:57 +0000

    separating the dev/stage/prod environments

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    DEBUG = False
+    Testing = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'THIS_KEY_SHOULD_BE_GENERATED_AND_UNIQUE'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    Testing = True
